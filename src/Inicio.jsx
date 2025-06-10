819886591442d8ad7f49a5f92c50853c92593526
Santi Padilla <smauriciop001@gmail.com>
2025-06-10 00:37:03 -0300

    feat and fix: Se implementa en la paguina de inicio el listado de productos y el poder editar los productos, tambien se corrige la conexion de VerDetalles.jsx con App.jsx [Padilla Santiago]

--- src/Inicio.jsx
+++ src/Inicio.jsx
@@ -1,4 +1,47 @@
-function Inicio() {
-  return <h2>Página de inicio</h2>;
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Badge from 'react-bootstrap/Badge';
+import { useNavigate } from "react-router-dom";
+
+/* el producto tiene los siguientes elementos, cuando se cree agregar producto eliminar estos comentarios
+    -id -imagen representativa -nombre -precio -descripcion -categoria -favorito
+    -estado (booleano)
+*/
+function Inicio({ productos }) {
+  const navigate = useNavigate();
+
+  const listaProductos = productos
+    .filter(producto => producto.estado)
+    .map((producto) => (
+      <div key={producto.id} className="col-md-4 d-flex mb-4">
+        <Card className="card w-100">
+          <Card.Img variant="top" src={producto.imagen || "holder.js/100px180?text=Image cap"} />
+          <Card.Body>
+            <Card.Title>{producto.nombre}</Card.Title>
+            <Card.Text>
+              Datos del producto ID: {producto.id}<br />
+              Descripcion: {producto.descripcion}
+            </Card.Text>
+            <Button variant="success" onClick={() => navigate(`/editar-producto/${producto.id}`)}>Editar</Button>{' '}
+            <Button variant="info" onClick={() => navigate(`/producto/${producto.id}`)}>Ver Detalles</Button>
+          </Card.Body>
+        </Card>
+      </div>
+    ));
+
+  return (
+    <div className="container">
+    <div className="d-flex justify-content-between align-items-center mb-3">
+      <h2><Badge bg="primary">Listado de productos</Badge></h2>
+    </div>
+    <div className="row">
+      {listaProductos.length > 0 ? listaProductos : (
+        <p className="text-muted">No hay productos activos en este momento.</p>
+      )}
+    </div>
+  </div>
+);
+
 }
+
 export default Inicio;
