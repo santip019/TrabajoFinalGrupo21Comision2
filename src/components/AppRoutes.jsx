89447a448e0afd0b6635625bff9e3e19b2d83268
Santi Padilla <smauriciop001@gmail.com>
2025-06-21 18:49:59 -0300

    feat: Se optimiza el navbar [Cruz Antonella, Efimov Valentina, Padilla Santiago]

--- src/components/AppRoutes.jsx
+++ src/components/AppRoutes.jsx
@@ -1,65 +1,53 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Layout from './AppNavbar';
-import Inicio from '../pages/Inicio';
-import Login from '../pages/Login';
-import NuevoProducto from './NuevoProducto';
-import Favoritos from '../pages/Favoritos';
-import AcercaDe from '../pages/AcercaDe';
-import EditarProducto from './EditarProducto';
-import VerDetalles from '../pages/VerDetalles';
-import Papelera from '../pages/Papelera';
+import Layout from "./AppNavbar";
+import Inicio from "../pages/Inicio";
+import Login from "../pages/Login";
+import NuevoProducto from "./NuevoProducto";
+import Favoritos from "../pages/Favoritos";
+import AcercaDe from "../pages/AcercaDe";
+import EditarProducto from "./EditarProducto";
+import VerDetalles from "../pages/VerDetalles";
+import Papelera from "../pages/Papelera";
 import AdminRoute from "../routes/AdminRoute";
 import RutaAutenticada from "./RutaAutenticada";
-import Soporte from '../pages/Soporte';
-import Devoluciones from './Devoluciones';
-import Faq from '../pages/Faq';
-import Contacto from './Contacto';
-import Seguridad from './Seguridad';
-import Cuenta from './Cuenta';
-import Carrito from '../pages/Carrito'
-
+import Soporte from "../pages/Soporte";
+import Devoluciones from "./Devoluciones";
+import Faq from "../pages/Faq";
+import Contacto from "./Contacto";
+import Seguridad from "./Seguridad";
+import Cuenta from "./Cuenta";
+import Carrito from "../pages/Carrito";
 
 function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/Layout" />} />
 
-      {/* Soporte técnico: RUTAS PÚBLICAS */}
-      <Route path="/soporte" element={<Soporte />} />
-      <Route path="/soporte/devoluciones" element={<Devoluciones />} />
-      <Route path="/soporte/faq" element={<Faq />} />
-      <Route path="/soporte/contacto" element={<Contacto />} />
-      <Route path="/soporte/seguridad" element={<Seguridad />} />
-      <Route path="/soporte/cuenta" element={<Cuenta />} />
-
       <Route path="/Layout" element={<Layout />}>
-
         <Route index element={<Inicio />} />
         <Route path="favoritos" element={<Favoritos />} />
+        <Route path="login" element={<Login />} />
+        <Route path="carrito" element={<Carrito />} />
+
+        
         <Route path="acerca-de" element={<AcercaDe />} />
         <Route path="papelera" element={<Papelera />} />
         <Route path="producto/:id" element={<VerDetalles />} />
-        <Route path="carrito" element={<Carrito />} />
+        
 
-        {/* Rutas solo para admin */}
-        <Route path="nuevo-producto" element={
-          <AdminRoute>
-            <NuevoProducto />
-          </AdminRoute>
-        } />
-        <Route path="editar-producto/:id" element={
-          <AdminRoute>
-            <EditarProducto />
-          </AdminRoute>
-        } />
+        <Route path="soporte" element={<Soporte />} />
+        <Route path="soporte/devoluciones" element={<Devoluciones />} />
+        <Route path="soporte/faq" element={<Faq />} />
+        <Route path="soporte/contacto" element={<Contacto />} />
+        <Route path="soporte/seguridad" element={<Seguridad />} />
+        <Route path="soporte/cuenta" element={<Cuenta />} />
+
       </Route>
-      
+
       {/* Redirección por defecto */}
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
-    
   );
-  
 }
 
 export default AppRoutes;
