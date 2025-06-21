a9938a296a3fd862f3716206bc0e23794ed761ba
Santi Padilla <smauriciop001@gmail.com>
2025-06-21 15:56:03 -0300

    chore: arreglar en general problemas por colision [Padilla Santiago]

--- src/assets/components/Cuenta.jsx
+++ src/assets/components/Cuenta.jsx
@@ -1,6 +1,6 @@
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 export default function Cuenta() {
   const navigate = useNavigate();
