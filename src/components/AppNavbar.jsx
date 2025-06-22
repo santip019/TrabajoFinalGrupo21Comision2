e3a5f32c2e7b7348d1b706dd2cba1a3b828d98bd
Valentina Efimov <valentinaefimov0811@gmail.com>
2025-06-22 20:49:44 -0300

    style: se modifica el diseño del navbar [Efimov Valentina]

--- src/components/AppNavbar.jsx
+++ src/components/AppNavbar.jsx
@@ -1,4 +1,8 @@
 import { Navbar, Container, Nav, Row, Col, Dropdown } from "react-bootstrap";
+import { LuSearch } from "react-icons/lu";
+import { FaRegStar, FaRegUser } from "react-icons/fa";
+import { MdOutlineShoppingCart } from "react-icons/md";
+import { MdOutlineContactSupport } from "react-icons/md";
 import { Link, Outlet } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
 import { useSelector } from "react-redux";
@@ -18,32 +22,44 @@ function Layout() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Container>
-              <Row className="w-100 align-items-center">
-                <Col md={4}>
+              <Row className="menu-arriba w-100 align-items-center ">
+                <Col md={3} className="d-flex justify-content-start ">
                   <Navbar.Brand as={Link} to="/">
                     Mi Logo
                   </Navbar.Brand>
                 </Col>
-              
-                <Col md={4} className="text-center">
-                  <Form className="d-flex ms-3">
-                  <Form.Control
-                    type="search"
-                    placeholder="Buscar producto..."
-                    className="me-2"
-                    value={busqueda}
-                    onChange={(e) => setBusqueda(e.target.value)}
-                    style={{ maxWidth: "200px" }}
-                  />
+                <Col md={5} className="d-flex justify-content-center ">
+                  <Form className="contenedor-busqueda d-flex w-100">
+                    <Form.Control
+                      type="search"
+                      placeholder="Buscar producto..."
+                      className="busqueda"
+                      value={busqueda}
+                      onChange={(e) => setBusqueda(e.target.value)}
+                    />
+                    <span className="icono-lupa">
+                      <LuSearch />
+                    </span>
                 </Form>
                 </Col>
-
-                <Col md={4} className="d-flex justify-content-end">
-                  <Nav className="me-auto">
+                <Col md={4} className="d-flex justify-content-end ">
+                  <Nav>
                     <Nav.Link as={Link} to="/Layout/favoritos">
+                      <span className="icono-estrella">
+                        <FaRegStar />
+                      </span>
                       Favoritos
                     </Nav.Link>
+                    <Nav.Link as={Link} to="/Layout/login">
+                      <span className="icono-cliente">
+                        <FaRegUser />
+                      </span>
+                      Cliente
+                    </Nav.Link>
                     <Nav.Link as={Link} to="/Layout/carrito">
+                      <span className="icono-carrito">
+                        <MdOutlineShoppingCart />
+                      </span>
                       Carrito
                       {cantidadTotal > 0 && (
                         <span className="badge bg-light text-dark ms-1">
@@ -53,43 +69,32 @@ function Layout() {
                     </Nav.Link>
                   </Nav>
                 </Col>
+              </Row>
 
-                <Col xs={12}>
-                  <Nav className="me-auto">
-                    <Dropdown>
-                      <Dropdown.Toggle
-                        variant="none"
-                        id="dropdown-categorias"
-                        className="categorias"
-                      >
-                        Categorías
-                      </Dropdown.Toggle>
-                      <Dropdown.Menu>
-                        <Dropdown.Item href="#categoria1">
-                          Categoría 1
-                        </Dropdown.Item>
-                        <Dropdown.Item href="#categoria2">
-                          Categoría 2
-                        </Dropdown.Item>
-                        <Dropdown.Item href="#categoria3">
-                          Categoría 3
-                        </Dropdown.Item>
-                      </Dropdown.Menu>
-                    </Dropdown>
-                    <Nav.Link as={Link} to="/Layout">
-                      Inicio
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/Layout/promociones">
-                      Promociones
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/Layout/mas-vendidos">
-                      Más Vendidos
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/Layout/novedades">
-                      Novedades
-                    </Nav.Link>
-                    <Nav.Link as={Link} to="/Layout/soporte">
-                      Ayuda
+              <Row className="menu-abajo w-100 align-items-center ">
+                <Col md={2} className="d-flex justify-content-start">
+                  <Dropdown>
+                    <Dropdown.Toggle variant="none" id="dropdown-categorias" className="categorias">
+                      Categorías
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="#categoria1"> Categoría 1 </Dropdown.Item>
+                      <Dropdown.Item href="#categoria2"> Categoría 2 </Dropdown.Item>
+                      <Dropdown.Item href="#categoria3"> Categoría 3 </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </Col>
+                <Col md={10} className="d-flex justify-content-end">
+                  <Nav>
+                    <Nav.Link as={Link} to="/Layout"> Inicio </Nav.Link>
+                    <Nav.Link as={Link} to="/Layout/promociones"> Promociones </Nav.Link>
+                    <Nav.Link as={Link} to="/Layout/mas-vendidos"> Más Vendidos </Nav.Link>
+                    <Nav.Link as={Link} to="/Layout/novedades"> Novedades </Nav.Link>
+                    <Nav.Link as={Link} to="/Layout/soporte"> 
+                      Ayuda 
+                      <span className="icono-ayuda">
+                        <MdOutlineContactSupport />
+                      </span>
                     </Nav.Link>
                   </Nav>
                 </Col>
